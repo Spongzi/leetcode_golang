669422078679ac9f2cfb7fbd1554259d00c1f3f2
Spongzi <lxlandsx@outlook.com>
2023-01-10 17:14:56 +0800

    链表 环形链表

---
+++ 链表/141. 环形链表/main.go
@@ -0,0 +1,26 @@
+package 环形链表
+
+/**
+https://leetcode.cn/problems/linked-list-cycle/
+*/
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func hasCycle(head *ListNode) bool {
+	if head == nil || head.Next == nil {
+		return false
+	}
+	slow := head
+	fast := head.Next
+	for fast != nil && fast.Next != nil {
+		if slow == fast {
+			return true
+		}
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+	return false
+}
