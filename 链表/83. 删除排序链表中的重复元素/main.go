ab871263be21c0c3e343526492760bf45b195c3a
Spongzi <lxlandsx@outlook.com>
2023-01-10 19:23:23 +0800

    链表 83 删除链表中的重复元素

---
+++ 链表/83. 删除排序链表中的重复元素/main.go
@@ -0,0 +1,32 @@
+package 删除排序链表中的重复元素
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func deleteDuplicates(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	head.Next = deleteDuplicates(head.Next)
+	if head.Val == head.Next.Val {
+		return head.Next
+	}
+	return head
+}
+
+func deleteDuplicatesIteration(head *ListNode) *ListNode {
+	if head == nil {
+		return head
+	}
+	cur := head
+	for cur.Next != nil {
+		if cur.Val == cur.Next.Val {
+			cur.Next = cur.Next.Next
+		} else {
+			cur = cur.Next
+		}
+	}
+	return head
+}
