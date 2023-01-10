5004d02ba7449359fd77d6cd551c599dcd96a096
Spongzi <lxlandsx@outlook.com>
2023-01-10 11:40:21 +0800

    添加迭代的做法

--- 链表/206.反转链表/main.go
+++ 链表/206.反转链表/main.go
@@ -18,3 +18,15 @@ func reverseList(head *ListNode) *ListNode {
 	head.Next = nil
 	return newHead
 }
+
+func reverseListIteration(head *ListNode) *ListNode {
+	var prev *ListNode
+	curr := head
+	for curr != nil {
+		next := curr.Next
+		curr.Next = prev
+		prev = curr
+		curr = next
+	}
+	return prev
+}
